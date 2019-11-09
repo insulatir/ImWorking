a1a9236e6a662d1099bb6d00f93ed570fcb1268a
insulator <s42415466@gmail.com>
2019-11-09 17:29:10 +0900

    test

--- maven-with-jenkins/scripts/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ ! -d /home/vagrant/$1 ];
-then
-  sudo mkdir /home/vagrant/$1
-fi
-sudo cp -f $WORKSPACE/target/*.jar /home/vagrant/$1
-cd /home/vagrant/$1
-sudo nohup java -Xmx128M -jar -Dapplication.name=$1 /home/vagrant/$1/*.jar &
